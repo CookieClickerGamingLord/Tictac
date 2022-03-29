2f6759511047ae04c6f107510f7988c92f0febb1
Aaro <s2000955@edu.bc.fi>
2022-03-29 10:01:25 +0300

    first commit

---
+++ t/Position.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t
+{
+    public class Position
+    { 
+        public int x { get; set; }
+
+        public int y { get; set; }
+    }
+}
